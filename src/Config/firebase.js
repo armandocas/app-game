64510395a1d360dbd0a2a69db11cac20b2aefb12
armandocas <armandoneto18@gmail.com>
2024-11-28 23:28:23 -0300

    chore: add envs

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1JF3g5oclUd6ajBc0bNvuS2DFwRvQJKc",
-  authDomain: "app-game-5c219.firebaseapp.com",
-  projectId: "app-game-5c219",
-  storageBucket: "app-game-5c219.appspot.com",
-  messagingSenderId: "863516791666",
-  appId: "1:863516791666:web:fbec39bed4b49771ce43eb",
-  measurementId: "G-T94EFF4K5V"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
