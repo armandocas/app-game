00dc6e963469fc1c13ea50155b85bbe45731d3f1
armando <armandoneto18@gmail.com>
2025-02-24 22:02:31 -0300

    chore: change new time

--- src/scheduler.mjs
+++ src/scheduler.mjs
@@ -56,7 +56,7 @@ function agendarAtualizacaoHistorico() {
     }
   });
 
-  schedule.scheduleJob("00 22 * * *", async () => {
+  schedule.scheduleJob("05 01 * * *", async () => {
     console.log("🚨 Executando a tarefa da Lotofácil...");
     try {
       await coletarDadosLotofacilPG();
